c0e72053f5bddac13474a8c2efc9517e02c3d2bf
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-13 10:32:09 +0530

    Refactor GetStartPage layout by updating text formatting, adding spacing, and implementing a 'Get Started' button with enhanced styling and functionality.

--- money_manager/lib/pages/getStartPage.dart
+++ money_manager/lib/pages/getStartPage.dart
@@ -9,30 +9,60 @@ class GetStartPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppColors.veryLightGray,
 
-      appBar: AppBar(backgroundColor: AppColors.veryLightGray),
-
       body: Center(
         child: Column(
           mainAxisAlignment:
               MainAxisAlignment.center, // Optional: centers content vertically
           children: [
             Text(
-              'Money Manager'.toUpperCase(),
+              'Money\nManager'.toUpperCase(),
+              textAlign: TextAlign.center,
               style: const TextStyle(
                 fontFamily: 'Roboto Mono',
                 color: AppColors.primaryOrange,
-                fontSize: 32,
+                fontSize: 35,
                 fontWeight: FontWeight.bold,
                 letterSpacing: 1.5,
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ), // Optional: adds spacing between text and image
+
+            const SizedBox(height: 80),
+
             Image.asset(
               'assets/getStart.png',
               width: 200, // Optional: set width
               height: 200, // Optional: set height
+            ),
+
+            const SizedBox(height: 80),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Get Started pressed!');
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primaryOrange, // Red background
+                  foregroundColor: Colors.white, // White text
+                  textStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    letterSpacing: 1,
+                  ),
+                  minimumSize: Size(
+                    double.infinity,
+                    48,
+                  ), // Full width, height 48
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12), // Rounded corners
+                  ),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 12,
+                  ), // Vertical padding
+                ),
+                child: Text('GET STARTED', textAlign: TextAlign.center),
+              ),
             ),
           ],
         ),
