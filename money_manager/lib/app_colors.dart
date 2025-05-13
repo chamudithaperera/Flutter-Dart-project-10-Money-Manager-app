6643fce1ba87ab561903676b25052f9931a3235e
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2025-05-13 09:35:38 +0530

    Refactor main.dart to simplify app structure and remove unnecessary comments, setting up a basic MaterialApp with a Scaffold and AppBar.

---
+++ money_manager/lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color color1 = Color(0xFFFA812F); // Custom color 1
+  static const Color color2 = Color(0xFFFFB22C); // Custom color 2
+  static const Color color3 = Color(0xFF2A4759); // Custom color 3
+  static const Color color4 = Color(0xFFDDDDDD); // Custom color 4
+  static const Color color5 = Color(0xFFEEEEEE); // Custom color 5
+}
